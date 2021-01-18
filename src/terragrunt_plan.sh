30946bc48ddf3f78465e7b325ad5be64dcb2009d
Vasyl Stetsuryn <vasyl@vasyl.org>
2021-01-18 13:15:14 +0200

    remove plan cuting off

--- src/terragrunt_plan.sh
+++ src/terragrunt_plan.sh
@@ -27,17 +27,12 @@ function terragruntPlan {
     echo "plan: info: successfully planned Terragrunt configuration in ${tfWorkingDir}"
     echo "${planOutput}"
     echo
-    if echo "${planOutput}" | egrep '^-{72}$' &> /dev/null; then
-        planOutput=$(echo "${planOutput}" | sed -n -r '/-{72}/,/-{72}/{ /-{72}/d; p }')
-    fi
+
     planOutput=$(echo "${planOutput}" | sed -r -e 's/^  \+/\+/g' | sed -r -e 's/^  ~/~/g' | sed -r -e 's/^  -/-/g')
 
     # If output is longer than max length (65536 characters), keep last part
     planOutput=$(echo "${planOutput}" | tail -c 65000 )
 
-    echo "DEBUG >>>>>>>"
-    echo "${planOutput}"
-    echo "DEBUG <<<<<<<"
   fi
 
   # Exit code of !0 indicates failure.
