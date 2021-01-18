c8df61f01ba7ac3124e344f51b50fc879128117e
Vasyl Stetsuryn <vasyl@vasyl.org>
2021-01-18 11:56:04 +0200

    Testing terragrunt feature in branch comment-plan-result

--- src/terragrunt_plan.sh
+++ src/terragrunt_plan.sh
@@ -7,7 +7,7 @@ function terragruntPlan {
   planExitCode=${?}
   planHasChanges=false
   planCommentStatus="Failed"
-  planResult=""
+  planResult=$(echo "${planOutput}" | grep 'Plan')
 
   # Exit code of 0 indicates success with no changes. Print the output and exit.
   if [ ${planExitCode} -eq 0 ]; then
@@ -34,7 +34,6 @@ function terragruntPlan {
 
     # If output is longer than max length (65536 characters), keep last part
     planOutput=$(echo "${planOutput}" | tail -c 65000 )
-    planResult=$(echo "${planOutput}" | grep Plan)
   fi
 
   # Exit code of !0 indicates failure.
@@ -56,6 +55,7 @@ ${planOutput}
 </details>
 
 *Plan result: \`${planResult}\`*
+
 *Workflow: \`${GITHUB_WORKFLOW}\`, Action: \`${GITHUB_ACTION}\`, Working Directory: \`${tfWorkingDir}\`, Workspace: \`${tfWorkspace}\`*"
 
     planCommentWrapper=$(stripColors "${planCommentWrapper}")
